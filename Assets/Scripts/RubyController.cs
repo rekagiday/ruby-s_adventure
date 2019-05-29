e3abf15bcba5c234e8a3af61abb7a29dd99ca40f
Reka Giday <giday.r@gmail.com>
2019-05-29 14:21:17 +0100

    Add Rigidbody to character and collider to various GameObjects

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    
     // Start is called before the first frame update
     void Start()
     {
-    
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,9 +17,10 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        
+        rigidbody2d.MovePosition(position);
     }
 }
