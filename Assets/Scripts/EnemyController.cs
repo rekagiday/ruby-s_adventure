69f5a55e8431bd87d5ba728877f3ff7e30e34527
Reka Giday <giday.r@gmail.com>
2019-05-29 21:05:52 +0100

    Add danger zone and moving enemy to scene

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+    private Rigidbody2D rigidbody2D;
+    private int direction = 1;
+    private float timer;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+        Vector2 position = rigidbody2D.position;
+        
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+        
+        rigidbody2D.MovePosition(position);
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController >();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,6 +8,9 @@ public class RubyController : MonoBehaviour
 
     public float speed = 3.0f;
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
     public int health { get { return currentHealth; }}
     int currentHealth;
     private Rigidbody2D rigidbody2d;
@@ -16,7 +19,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = 1;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -29,10 +32,25 @@ public class RubyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
         
         rigidbody2d.MovePosition(position);
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
